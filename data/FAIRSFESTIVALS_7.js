ff466198519d67789a5744bdc52b62af4a58b853
Viranchdave <76239614+Viranchdave@users.noreply.github.com>
2021-02-26 20:58:08 +0530

    Add files via upload

---
+++ data/FAIRSFESTIVALS_7.js
@@ -0,0 +1,10 @@
+var json_FAIRSFESTIVALS_7 = {
+"type": "FeatureCollection",
+"name": "FAIRSFESTIVALS_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "-": "D:\/Desktop\/tp\/Screenshot_20201018-163448_2.jpg", "NAME": "Beneshwar Fair", "SITE TYPE": "Cultural\/Religious\/Historical", "LOCATION": "Beneshwar Dham Dungarpur district", "DETAILS": "At the confluence point of Mahi,Jakham & Som rivers is major centre of Cultural & religious significance of Wagad. Popularly known as BeneshwarDham is a small island which hosts the world's largest tribal fair every year in the month of Febuary." }, "geometry": { "type": "Point", "coordinates": [ 74.192158377236296, 23.799711006508371 ] } },
+{ "type": "Feature", "properties": { "-": "D:\/Desktop\/tp\/Picture1.jpg", "NAME": "Dusshera fair", "SITE TYPE": "Cultural\/Religious", "LOCATION": "Banswara city", "DETAILS": "In the centre of Banswara city Kushalbagh ground occupy a prominent place which showcases many important events throughout the year which attracts large number of people. Of all such events Dusshera fair happens during Navratri every year" }, "geometry": { "type": "Point", "coordinates": [ 74.443355999302497, 23.548702399356578 ] } },
+{ "type": "Feature", "properties": { "-": "D:\/Desktop\/tp\/Picture3.png", "NAME": "Ghotia Amba", "SITE TYPE": "Cultural\/Religious\/Historical", "LOCATION": "Ratan pura Galiya village Banswara Sub district", "DETAILS": "Surrounded by enigmatic hills is a place called GhotiaAmba. According to mythology Pandavas stayed here during their exile & planted Mango trees (Amba) by which the name is derived. Every year a huge fair takes place during chaitra Navratri" }, "geometry": { "type": "Point", "coordinates": [ 74.41066654505731, 23.345398467020729 ] } }
+]
+}
